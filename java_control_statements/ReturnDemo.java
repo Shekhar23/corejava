7b2edd88c6e6a03d21d181a6726388765154ddab
Shekhar23 <spande6@gmail.com>
2018-08-06 05:33:28 +0530

    - Update example of return statement.

--- java_control_statements/ReturnDemo.java
+++ java_control_statements/ReturnDemo.java
@@ -1,7 +1,14 @@
 package java_control_statements;
+
 /**
  * @author shekhar 02-Aug-2018 2018 7:02:26 AM
  */
 public class ReturnDemo {
-
+	public static void main(String args[]) {
+		boolean t = true;
+		System.out.println("Before the return.");
+		if (t)
+			return; // return to caller
+		System.out.println("This won't execute.");
+	}
 }
